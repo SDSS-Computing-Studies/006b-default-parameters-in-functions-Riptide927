1b78bbbc18e3df1b526b596f795b6ea173bcc7c3
Peter <PeterC499@deltalearns.ca>
2020-10-14 11:16:06 -0700

    gggg

--- assignment.py
+++ assignment.py
@@ -14,15 +14,25 @@ def factorPair(x,y):
     lis.sort()
     return lis
 
-def cosineLaw():
-    print("text")
+def cosineLaw(A,B,C,oppositeSide=True):
+    if oppositeSide == False:
+       return solution(quadratic(1,B,C))
+    else:
+        c = math.sqrt((A**2)+(B**2)-(2*A*B*math.cos(toRadians(C))))
+        return c
+          
+def toRadians(x):
+    return ((x*math.pi)/180)
 
-def toRadians():
-    print("text")
+def solution(n):
+    if n[0] > 0:
+        return n[0]
+    else:
+        return n[1]
 
-def solution():
-    print("text")
-
-def quadratic():
-    print("text")
-
+def quadratic(a,b,c):
+    x1 = (-1*b + math.sqrt(b**2 - 4 * a*c)) / (2*a)
+    x2 = (-1*b - math.sqrt(b**2 - 4 * a*c)) / (2*a)
+    z = [x1,x2]
+    z.sort()
+    return(z)
--- extension.py
+++ extension.py
@@ -26,7 +26,116 @@ input parameter. It's return value is either:
 "O" (O is a winner)
 
 """
+def win(n):
+    if n == b[0] and n== b[1]  and n ==b[2] :
+        print(n,"Has won the game")
+        winner = True
+        return winner
+    elif n == b[3]  and n== b[4] and n == b[5] :
+        print(n,"Has won the game")
+        winner = True
+        return winner
+    elif n == b[6]  and n== b[7]  and n == b[8]:
+        print(n,"Has won the game")
+        winner = True
+        return winner
+    elif n == b[0] and n== b[3] and n ==b[6]:
+        print(n,"Has won the game")
+        winner = True
+        return winner
+    elif n == b[1]  and n== b[4]  and n ==b[7] :
+        print(n,"Has won the game")
+        winner = True
+        return winner
+    elif n == b[2]  and n== b[5]  and n ==b[8] :
+        print(n,"Has won the game")
+        winner = True
+        return winner
+    elif n == b[0]  and n== b[4]  and n ==b[8] :
+        print(n,"Has won the game")
+        winner = True
+        return winner
+    elif n == b[2]  and n== b[4]  and n ==b[6] :
+        print(n,"Has won the game")
+        winner = True
+        return winner
 
-def winner(game):
-    # input: list that contains 10 elements
-    # output: either 0, "X", "O" to indicate who the winner is.
+"""
+#!python3
+b = ["1","2","3","4","5","6","7","8","9"]
+winner = False
+def board():
+    import os
+    os.system("cls")
+    print(b[0]+b[1]+b[2]+ "\n" + b[3]+b[4]+b[5] + "\n"+b[6]+b[7]+b[8])
+
+def turns():
+    t=0
+    while t < 9:
+        turn1=""
+        turn2=""
+        while turn1 != "over":
+            m = int(input("It is x's turn \n where do you want your x "))
+            if b[m-1] == "o":
+                print("sorry that spot is taken")
+            else:
+                b[m-1]= "x"
+                turn1 = "over"
+                board()
+                t+=1
+        if win("x")==True:
+            break
+        if t== 9:
+            print("Nobody wins you all suck")
+            break
+        while turn2 != "over":
+            m = int(input("It is o's turn \n where do you want your o "))
+            if b[m-1] == "x":
+                print("sorry that spot is taken")
+            else:
+                b[m-1]= "o"
+                turn2 = "over"
+                board()
+                t+=1
+        if win("o")==True:
+            break
+       
+def win(n):
+    if n == b[0] and n== b[1]  and n ==b[2] :
+        print(n,"Has won the game")
+        winner = True
+        return winner
+    elif n == b[3]  and n== b[4] and n == b[5] :
+        print(n,"Has won the game")
+        winner = True
+        return winner
+    elif n == b[6]  and n== b[7]  and n == b[8]:
+        print(n,"Has won the game")
+        winner = True
+        return winner
+    elif n == b[0] and n== b[3] and n ==b[6]:
+        print(n,"Has won the game")
+        winner = True
+        return winner
+    elif n == b[1]  and n== b[4]  and n ==b[7] :
+        print(n,"Has won the game")
+        winner = True
+        return winner
+    elif n == b[2]  and n== b[5]  and n ==b[8] :
+        print(n,"Has won the game")
+        winner = True
+        return winner
+    elif n == b[0]  and n== b[4]  and n ==b[8] :
+        print(n,"Has won the game")
+        winner = True
+        return winner
+    elif n == b[2]  and n== b[4]  and n ==b[6] :
+        print(n,"Has won the game")
+        winner = True
+        return winner
+
+
+
+board()
+turns()
+"""
